a4d7246e131cdd4c8da18b5c15911d42ef5c5d4b
DharmaTeja Vytla <teja6d@gmail.com>
2024-06-29 12:19:19 +0100

    refactor: Update serviceFactory.ts to use correct import for FakeUserService

--- src/config/serviceFactory.ts
+++ src/config/serviceFactory.ts
@@ -1,14 +1,13 @@
 import { IUser } from "../models/user/IUser";
 import { IUserService } from "../models/user/IUserService";
-import { FakeUserService } from "../services/fakeservices/fakeUserservices/fakeUserServices";
+import { FakeUserService } from "../services/fakeservices/fakeUserService";
 import { UserService } from "../services/userservices/userService";
 
 export default function getUserService(): IUserService {
     const useFake = process.env.USE_FAKE_SERVICES === "true";
-    const fakesData = [{ username: "testuser", password: "testpassword", email: "test@gmail.com" }] as IUser[]
     if (useFake) {
-        return new FakeUserService(fakesData);
+        return new FakeUserService();
     } else {
-        return new UserService(); // Assuming RealUserService is your actual service implementation
+        return new UserService();
     }
 }
